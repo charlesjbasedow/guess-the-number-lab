345ba59cc55d9be96c76f17d17310ec8be2b470c
charlesjbasedow <cbasedow@outlook.com>
2022-02-01 00:45:13 -0500

    Guess The Number Lab

--- app.js
+++ app.js
@@ -4,8 +4,39 @@ const game = {
   smallestNum: 1,
   secretNum: null,
   prevGuesses: [], 
+  
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
-  }
+      
+    do {
+        this.prevGuesses.push(this.getGuess())
+        this.render()
+      
+    } while (this.prevGuesses[this.prevGuesses.length - 1] !== this.secretNum);
+  },
+
+  getGuess: function(){
+    let playerGuess = null
+    
+    do {
+      playerGuess = parseInt(prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`))
+
+    } while (isNaN(playerGuess) 
+    || playerGuess < this.smallestNum || playerGuess > this.biggestNum)
+    return playerGuess
+  },
+
+  render: function(){
+    let message = null
+    
+    if (this.prevGuesses[this.prevGuesses.length - 1] === this.secretNum) {
+    message = `Congrats! You guessed the number in ${this.prevGuesses.length} ${this.prevGuesses.length > 1 ? 'guesses' : 'guess'}!`
+    
+    } else {
+    message = `Your guess is too ${this.prevGuesses[this.prevGuesses.length -1] > this.secretNum ? 'high' : 'low'}. Previous guesses: ${this.prevGuesses.join(', ')} `        }
+    alert(message)
+  }, 
 }
+game.play()
+console.log(game.secretNum)
